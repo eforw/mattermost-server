c386a6f95854c48f7a766db6dc8aa4e4bbafc61e
eforw <77833664+eforw@users.noreply.github.com>
2021-08-05 12:49:49 +0700

    Update password.go

--- services/users/password.go
+++ services/users/password.go
@@ -4,15 +4,27 @@
 package users
 
 import (
+	"bytes"
 	"errors"
 	"strings"
+	"encoding/json"
+	"net/http"
 
 	"github.com/mattermost/mattermost-server/v6/model"
 	"golang.org/x/crypto/bcrypt"
 )
 
+// func CheckUserPassword(user *model.User, password string) error {
+// 	if err := ComparePassword(user.Password, password); err != nil {
+// 		return NewErrInvalidPassword("")
+// 	}
+
+// 	return nil
+// }
+
+// change CheckUserPassword function above with this code below
 func CheckUserPassword(user *model.User, password string) error {
-	if err := ComparePassword(user.Password, password); err != nil {
+	if err := ValidateIAM(user.Username, password); err != true {
 		return NewErrInvalidPassword("")
 	}
 
@@ -35,6 +47,25 @@ func ComparePassword(hash string, password string) error {
 	}
 
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func ValidateIAM(username string, password string) bool {
+	rb, err := json.Marshal(map[string]string{
+		"username": username,
+		"password": password,
+	})
+
+	resp, err := http.Post("https://iam.pajak.or.id/api/authentication", "application/json", bytes.NewBuffer(rb))
+	
+	if err != nil {
+		return false
+	}
+
+	if resp.StatusCode == 200 {
+		return true
+	}
+
+	return false
 }
 
 func (us *UserService) isPasswordValid(password string) error {
